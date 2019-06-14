c4c57b6ab84902bc5290f0195b6158503f451f35
Seth Nielsen <sethmnielsen@gmail.com>
2019-06-14 09:49:39 -0600

    Working on keyboard control

--- src/rosflight_joy/rosflight_keyboard_base.py
+++ src/rosflight_joy/rosflight_keyboard_base.py
@@ -11,18 +11,14 @@ class rosflight_keyboard_base():
     def __init__(self, device=0):
         os.environ["SDL_VIDEODRIVER"] = "dummy"
         pygame.display.init()
-        screen = pygame.display.set_mode((1, 1))
-        pygame.joystick.init()
+        screen = pygame.display.set_mode((100, 100))
 
-        self.joy = pygame.joystick.Joystick(device)
-        self.joy.init()
-
-        print("joystick: {} (axes: {}, buttons: {}, hats: {})".format(
-            self.joy.get_name(), self.joy.get_numaxes(), self.joy.get_numbuttons(), self.joy.get_numhats()))
+        print("pygame with keyboard control")
 
         self.mapping = dict()
 
         self.look_for_button_press_events = False
+        self.keypress = False
 
         self.values = dict()
         self.values['x'] = 0
@@ -36,68 +32,22 @@ class rosflight_keyboard_base():
 
         self.next_update_time = time.time()
 
-        if 'Taranis' in self.joy.get_name():
-            print("found Taranis")
-            self.mapping['x'] = 1
-            self.mapping['y'] = 2
-            self.mapping['z'] = 3
-            self.mapping['F'] = 0
-            self.mapping['xsign'] = 1
-            self.mapping['ysign'] = -1
-            self.mapping['zsign'] = 1
-            self.mapping['Fsign'] = 1
-            self.mapping['aux1'] = {'type': 'axis', 'id': 4}
-            self.mapping['aux2'] = {'type': 'axis', 'id': 5}
-            self.mapping['aux3'] = {'type': 'axis', 'id': 6}
-            self.mapping['aux4'] = {'type': 'switch', 'id': 2}
+        self.mapping['x'] = 1  # R - L/R
+        self.mapping['y'] = 2  # R - U/D
+        self.mapping['z'] = 3  # L - L/R
+        self.mapping['F'] = 0  # L - U/D
+        self.mapping['xsign'] = 1
+        self.mapping['ysign'] = -1
+        self.mapping['zsign'] = 1
+        self.mapping['Fsign'] = 1
+        self.mapping['aux1'] = {'type': 'axis', 'id': 4}
+        self.mapping['aux2'] = {'type': 'axis', 'id': 5}
+        self.mapping['aux3'] = {'type': 'axis', 'id': 6}
+        self.mapping['aux4'] = {'type': 'switch', 'id': 2}
 
-        elif 'Xbox' in self.joy.get_name() or 'X-Box' in self.joy.get_name():
-            # print "found xbox"
-            self.mapping['x'] = 3
-            self.mapping['y'] = 4
-            self.mapping['z'] = 0
-            self.mapping['F'] = 1
-            self.mapping['xsign'] = 1
-            self.mapping['ysign'] = 1
-            self.mapping['zsign'] = 1
-            self.mapping['Fsign'] = -1
-            self.mapping['aux1'] = {'type': 'button', 'id': 0}
-            self.mapping['aux2'] = {'type': 'button', 'id': 1}
-            self.mapping['aux3'] = {'type': 'button', 'id': 2}
-            self.mapping['aux4'] = {'type': 'button', 'id': 3}
-            self.look_for_button_press_events = True
+        self.keys_increase = [pygame.K_RIGHT, pygame.K_UP, pygame.K_d, pygame.K_w]
 
-        elif 'Extreme 3D' in self.joy.get_name():
-            # print "found Logitech Extreme 3D"
-            self.mapping['x'] = 0
-            self.mapping['y'] = 1
-            self.mapping['z'] = 2
-            self.mapping['F'] = 3
-            self.mapping['xsign'] = 1
-            self.mapping['ysign'] = 1
-            self.mapping['zsign'] = 1
-            self.mapping['Fsign'] = -1
-            self.mapping['aux1'] = {'type': 'button', 'id': 0}
-            self.mapping['aux2'] = {'type': 'button', 'id': 1}
-            self.mapping['aux3'] = {'type': 'button', 'id': 2}
-            self.mapping['aux4'] = {'type': 'button', 'id': 3}
-            # the Extreme 3D has actually 12 buttons, but rc_joy forwards only 4 aux keys
-            self.look_for_button_press_events = True
-
-        else:
-            print("using realflight mapping")
-            self.mapping['x'] = 1
-            self.mapping['y'] = 2
-            self.mapping['z'] = 4
-            self.mapping['F'] = 0
-            self.mapping['xsign'] = 1
-            self.mapping['ysign'] = 1
-            self.mapping['zsign'] = 1
-            self.mapping['Fsign'] = 1
-            self.mapping['aux1'] = {'type': 'axis', 'id': 3}
-            self.mapping['aux2'] = {'type': 'switch', 'id': 0}
-            self.mapping['aux3'] = {'type': 'switch', 'id': 1}
-            self.mapping['aux4'] = {'type': 'switch', 'id': 2}
+        self.keys_decrease = [pygame.K_LEFT, pygame.K_DOWN, pygame.K_a, pygame.K_s, pygame.K_o]
 
         if self.look_for_button_press_events:
             # get keys for all inputs typed as 'button'
@@ -108,7 +58,6 @@ class rosflight_keyboard_base():
             for key in self.button_keys:
                 button_value = self.joy.get_button(self.mapping[key]['id'])
                 self.prev_button_values.append(button_value)
-                self.mapping[key]['value'] = button_value
 
     def update(self):
         pygame.event.pump()
@@ -134,26 +83,38 @@ class rosflight_keyboard_base():
         # For 'switch' type aux keys, the published value is directly the current state of being pressed.
         # Updated in below loop. Same for 'axis' type aux keys.
 
+        
+                # self.keypress = True
+
         # 50 Hz update
         if time.time() > self.next_update_time:
             self.next_update_time += 0.02
 
-            self.values['x'] = self.joy.get_axis(
-                self.mapping['x']) * self.mapping['xsign']
-            self.values['y'] = self.joy.get_axis(
-                self.mapping['y']) * self.mapping['ysign']
-            self.values['F'] = self.joy.get_axis(
-                self.mapping['F']) * self.mapping['Fsign']
-            self.values['z'] = self.joy.get_axis(
-                self.mapping['z']) * self.mapping['zsign']
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        print("Pressing down!")
+                if event.type == pygame.KEYUP:
+                    print('\nkey up: {}, {}\n'.format(event.key, pygame.key.name(event.key)))
+
+            self.values['x'] = self.mapping['xsign']
+            self.values['y'] = self.mapping['ysign']
+            self.values['F'] = self.mapping['Fsign']
+            self.values['z'] = self.mapping['zsign']
+
 
             for key in ['aux1', 'aux2', 'aux3', 'aux4']:
                 if self.mapping[key]['type'] == 'axis':
-                    self.values[key] = self.joy.get_axis(
-                        self.mapping[key]['id'])
+                    self.values[key] = 10
+                    # self.values[key] = self.joy.get_axis(
+                        # self.mapping[key]['id'])
                 elif self.mapping[key]['type'] == 'switch':
-                    self.values[key] = 2.0 * \
-                        self.joy.get_button(self.mapping[key]['id']) - 1.0
+                    self.values[key] = 1
+                    # self.values[key] = 2.0 * \
+                    #     self.joy.get_button(self.mapping[key]['id']) - 1.0
+
+            self.keypress = False
             return True
         else:
             return False
